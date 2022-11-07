6670cfc461f83a0997d8110829c4bdfcfe1fd583
guenk <gueguenk@gmail.com>
2022-11-07 14:06:20 +0100

    PUT

--- app.js
+++ app.js
@@ -34,4 +34,6 @@ app.listen(port, (err) => {
 
 app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", movieHandlers.postUser);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.put("/api/users/:id", movieHandlers.updateUser);
 
